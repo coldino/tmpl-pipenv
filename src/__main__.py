9f349612cf2e4874c39b6723af2af7ee310644b4
coldino <28345893+coldino@users.noreply.github.com>
2020-02-14 22:56:17 +0000

    Initial commit

---
+++ src/__main__.py
@@ -0,0 +1,9 @@
+from .stuff import do_stuff
+
+
+def main():
+    do_stuff()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_example.py
@@ -0,0 +1,2 @@
+def test_asserts():
+    assert 2 / 2 == 1, "Something doesn't add up"
